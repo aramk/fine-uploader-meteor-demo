288bc0cf7fdef1a616a1fd87e0bf671acbd63edc
Aram Kocharyan <akarmenia@gmail.com>
2017-05-14 17:10:29 +1000

    Add solution for uploading to S3 server-side

--- client/index.js
+++ client/index.js
@@ -1,16 +1,15 @@
 window.qq = require('../node_modules/fine-uploader/fine-uploader/fine-uploader.js');
 
 $(() => {
-  // Some options to pass to the uploader are discussed on the next page
   const uploader = new qq.FineUploader({
     element: $('#uploader')[0],
     debug: true,
     request: {
-      endpoint: 'http://localhost:8000/s3handler'
+      endpoint: 'http://localhost:8000/uploads'
     },
     deleteFile: {
       enabled: true,
-      endpoint: 'http://localhost:8000/s3handler'
+      endpoint: 'http://localhost:8000/uploads'
     }
   });
 });
